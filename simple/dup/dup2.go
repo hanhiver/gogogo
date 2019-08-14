15996be98dfefe056494a5ac52ddae30a8c0f144
hanhiver <13910395157@139.com>
2019-08-14 20:17:47 +0800

    update

---
+++ simple/dup/dup2.go
@@ -0,0 +1,43 @@
+// dup2 打印输入中多次出现的行的个数和文本。
+// 从stdin或者制定的文件列表读取。
+
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+)
+
+func main() {
+    counts := make(map[string]int)
+    files := os.Args[1:]
+
+    if len(files) == 0 {
+        for _, arg := range files {
+            f, err := os.Open(arg)
+            if err != nil {
+                fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+                continue
+            }
+            countLines(f, counts)
+            f.Close()
+        }
+    }
+
+    for line, n := range counts {
+        if n > 1 {
+            fmt.Printf("%d\t%s\n", n, line)
+        }
+    }
+}
+
+func countLines(f *os.File, counts map[string]int) {
+    input := bufio.NewScanner(f)
+    for input.Scan() {
+        fmt.Println(input.Text())
+        counts[input.Text()]++
+    }
+}
+
+
