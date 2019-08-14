0095ae9187673ad3d8cf72cf3ac8fe08a4af2c33
Dong Han <handongfr@gmail.com>
2019-08-14 23:47:25 +0800

    update

--- simple/dup/dup2.go
+++ simple/dup/dup2.go
@@ -10,17 +10,25 @@ import (
 )
 
 func main() {
+	fmt.Printf("Main: i am in.\n")
     counts := make(map[string]int)
     files := os.Args[1:]
 
+	fmt.Printf("%d files.\n", len(files))
+
     if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
         for _, arg := range files {
+			fmt.Printf("Handle the first file %s.\n", arg)
             f, err := os.Open(arg)
             if err != nil {
                 fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
                 continue
             }
+			fmt.Printf("File %s openned.\n", arg)
             countLines(f, counts)
+			fmt.Printf("File %s will be closed.\n", arg)
             f.Close()
         }
     }
