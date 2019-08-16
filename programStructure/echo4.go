d4f449b8f51de84f0b7945a63a80b0b1ef4fd56c
Dong Han <handongfr@gmail.com>
2019-08-16 11:41:26 +0800

    update

---
+++ programStructure/echo4.go
@@ -0,0 +1,19 @@
+// echo4输出命令行参数
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
+var n = flag.Bool("n", false, "omit trailing new line")
+var sep = flag.String("s", " ", "separator")
+
+func main() {
+	flag.Parse()
+	fmt.Print(strings.Join(flag.Args(), *sep))
+	if !*n {
+		fmt.Println()
+	}
+}
