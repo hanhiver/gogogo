b8df401c2466659c0e27def3afe16f72d170c6e2
hanhiver <13910395157@139.com>
2019-08-19 16:49:05 +0800

    添加变长函数例子

--- function/variableLengthFunc/varibleLengthFunc.go
+++ function/variableLengthFunc/varibleLengthFunc.go
@@ -11,6 +11,12 @@ func main() {
 	fmt.Println(sum(3))
 	fmt.Println(sum(1, 2, 3, 4, 5))
 
+	values := []int{1, 2, 3, 4, 5}
+	// 如果所有的参数已经放到一个列表中，调用的时候添加...
+	fmt.Println(sum(values...))
+
+	// 变长函数的类型和带有普通slice参数的函数不同。
+	fmt.Printf("typeof(ff) = %#v \ntypeof(gg) = %#v \n", ff, gg)
 }
 
 func sum(vals ...int) int {
@@ -21,3 +27,6 @@ func sum(vals ...int) int {
 
 	return total
 }
+
+func ff(...int) {}
+func gg([]int)  {}
