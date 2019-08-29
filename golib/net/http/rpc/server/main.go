ac1daa15d52cd85c0098380b78759f13d14f4e59
Dong Han <handongfr@gmail.com>
2019-08-29 13:54:22 +0800

    update

---
+++ golib/net/http/rpc/server/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"./server"
+	"fmt"
+	"gogogo/golib/net/http/rpc/server"
+	"http/rpc"
+)
+
+func main() {
+	arith := new(server.arith)
+
+}
---
+++ golib/net/http/rpc/server/rpcServer.go
@@ -0,0 +1,27 @@
+package server
+
+type Args struct {
+	A, B int
+}
+
+type Quotient struct {
+	Quo, Rem int
+}
+
+type Arith int
+
+func (t *Arith) Multiply(args *Args, reply *int) error {
+	*reply = args.A * args.B
+	return nil
+}
+
+func (t *Arith) Divide(args *Args, quo *Quotient) error {
+	if args.B == 0 {
+		quo.Quo = 0
+		quo.Rem = 0
+		return nil
+	}
+	quo.Quo = args.A / args.B
+	quo.Rem = args.A % args.B
+	return nil
+}
